417d478db239d7da74a23b7a1e34aa6ba34882f4
Luiz Praxedes <lhpsilva4@outlook.com>
2026-08-21 19:18:00 -0300

    chore: remove obsolete certification patch

--- cert-ui-patch.js
+++
@@ -1,6 +0,0 @@
-(function(){
-'use strict';
-var app=document.getElementById('app');
-function dedupe(){var sections=app.querySelectorAll('.cert-section'),seen={};for(var i=sections.length-1;i>=0;i--){var cards=sections[i].querySelectorAll('.cert-card');for(var j=0;j<cards.length;j++){var h=cards[j].querySelector('h3');if(!h)continue;var name=h.textContent.trim();if(seen[name])cards[j].remove();else seen[name]=true;}}}
-new MutationObserver(function(){dedupe();}).observe(app,{childList:true,subtree:true});
-})();
